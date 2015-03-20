638a11a13b1ec48e7fe1aeafaf6dbda69cd1a92f
Alastair Paragas <alastairparagas@gmail.com>
2015-03-19 22:34:13 -0400

    Finalized routes file

--- index.php
+++ index.php
@@ -3,21 +3,26 @@
 require('vendor/autoload.php');
 
 header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=utf-8");
 
 map('GET', '/animals', function() {
-    
+    $apiCall = new \Pttr\APICall();
+    $apiCall->getAnimals();
 });
 
-map('GET', '/animal', function() {
-    
+map('GET', '/animal/<id>', function($params) {
+    $apiCall = new \Pttr\APICall();
+    $apiCall->getAnimal($params['id']);
 });
 
 map('GET', '/shelters', function() {
-    
+    $apiCall = new \Pttr\APICall();
+    $apiCall->getShelters();
 });
 
-map('GET', '/shelter', function() {
-    
+map('GET', '/shelter/<id>', function($params) {
+    $apiCall = new \Pttr\APICall();
+    $apiCall->getShelter($params['id']);
 });
 
 config('url', "http://localhost/pttrv2-backend/");
