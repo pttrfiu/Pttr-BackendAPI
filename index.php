697d58c7015e5e062f7612fe619b2bf23b871ad9
Alastair Paragas <alastairparagas@gmail.com>
2015-03-21 20:37:04 -0400

    Handle errors and unify output to one function

--- index.php
+++ index.php
@@ -7,23 +7,45 @@ header("Content-Type: application/json; charset=utf-8");
 
 map('GET', '/animals', function() {
     $apiCall = new \Pttr\APICall();
-    $apiCall->getAnimals();
+    echo processOutput($apiCall->getAnimals());
 });
 
 map('GET', '/animal/<id>', function($params) {
     $apiCall = new \Pttr\APICall();
-    $apiCall->getAnimal($params['id']);
+    echo processOutput($apiCall->getAnimal($params['id']));
 });
 
 map('GET', '/shelters', function() {
     $apiCall = new \Pttr\APICall();
-    $apiCall->getShelters();
+    echo processOutput($apiCall->getShelters());
 });
 
 map('GET', '/shelter/<id>', function($params) {
     $apiCall = new \Pttr\APICall();
-    $apiCall->getShelter($params['id']);
+    echo processOutput($apiCall->getShelter($params['id']));
 });
+
+map(404, function() {
+    echo processOutput('', 'error', '404 - That API endpoint does not exist');
+});
+
+function processOutput($output = '', $status = 'ok', $message = '') {
+    if ($status == 'ok' && is_array($output)) {
+        return json_encode(array(
+            'status' => 'ok',
+            'message' => $message,
+            'results' => $output,
+            'count' => count($output)
+        ));
+    } else {
+        return json_encode(array(
+            'status' => $status,
+            'message' => $message,
+            'results' => $output,
+            'count' => '0'
+        ));
+    }
+}
 
 config('url', "http://localhost/pttrv2-backend/");
 
